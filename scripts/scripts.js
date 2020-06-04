363dac9fcec51e288edd4d4c1711db91bff5303f
John Botero <jhondark@gmail.com>
2020-06-03 22:51:11 -0500

    header, main section done

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+CustomElementRegistry.define('product-item', Product)
+
+class Product extends HTMLParagraphElement {
+    constructor() {
+        super()
+
+        document.querySelectorAll('Product')
+        
+    }
+}
